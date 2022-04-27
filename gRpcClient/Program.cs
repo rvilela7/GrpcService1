1435a1ac730213593e90f8a5ba18f91ee409c078
Rui Vilela <ruivil@gmail.com>
2022-04-27 15:56:43 +0100

    FirstCommit

---
+++ gRpcClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Net.Client;
+using GrpcService1;
+using System;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:49153/");
+            var client = new Greeter.GreeterClient(channel);
+            var input = new HelloRequest { Name = "Maria" };
+            var reply = await client.SayHelloAsync(input);
+
+            Console.WriteLine(reply.Message);
+
+            Console.ReadLine();
+        }
+    }
+}
+
+// Nuggets
+// Google.protobuf
+// Grpc.net.client
+// Grpc.tools
+
+// Property files for *proto (Client only)
