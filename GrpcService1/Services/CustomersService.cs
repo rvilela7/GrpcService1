196830266c2540ae34d4d8cb2971ceebfcb3f61c
Rui Vilela <ruivil@gmail.com>
2022-04-28 11:00:25 +0100

    CustomService

---
+++ GrpcService1/Services/CustomersService.cs
@@ -0,0 +1,36 @@
+﻿using Grpc.Core;
+
+namespace GrpcService1.Services
+{
+    public class CustomersService : Customer.CustomerBase //Notice BASE
+    {
+        private readonly ILogger<CustomersService> logger;
+        public CustomersService(ILogger<CustomersService> logger)
+        {
+            this.logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel output = new CustomerModel();
+
+            if (request.UserId == 1)
+            {
+                output.FirstName = "Jamie";
+                output.LastName = "Smith";
+            }
+            else if (request.UserId == 2)
+            {
+                output.FirstName = "Jane";
+                output.LastName = "Doe";
+            }
+            else
+            {
+                output.FirstName = "Greg";
+                output.LastName = "Thomas";
+            }
+
+            return Task.FromResult(output);
+        }
+    }
+}
--- gRpcClient/Program.cs
+++ gRpcClient/Program.cs
@@ -1,6 +1,6 @@
-﻿using Grpc.Net.Client;
+﻿
+using Grpc.Net.Client;
 using GrpcService1;
-using System;
 
 namespace GrpcClient
 {
@@ -8,21 +8,18 @@ namespace GrpcClient
     {
         static async Task Main(string[] args)
         {
-            var channel = GrpcChannel.ForAddress("http://localhost:49154/");
-            var client = new Greeter.GreeterClient(channel);
-            var input = new HelloRequest { Name = "Maria" };
-            var reply = await client.SayHelloAsync(input);
+            var channel = GrpcChannel.ForAddress("http://localhost:49160");
+            var customerClient = new Customer.CustomerClient(channel);
 
-            Console.WriteLine(reply.Message);
+            var rnd = new Random();
 
+            for (int i = 0; i < 10; i++)
+            {
+                var clientRequested = new CustomerLookupModel() { UserId = rnd.Next(3) + 1 };
+                var reply = await customerClient.GetCustomerInfoAsync(clientRequested);
+                Console.WriteLine($"{reply.FirstName} {reply.LastName}");
+            }
             Console.ReadLine();
         }
     }
 }
-
-// Nuggets
-// Google.protobuf
-// Grpc.net.client
-// Grpc.tools
-
-// Property files for *proto (Client only)
